30222458ea3b6da24e89a9eaaa237b5c03e4e401
narniaeagle <ilgar2023@hotmail.com>
2023-02-13 16:42:32 -0500

    done

---
+++ homework/src/App.js
@@ -0,0 +1,76 @@
+import './App.css';
+import Receipt from './components/receipt'
+const receipts = [
+  {
+    id: 1,
+    person: 'Jeremy',
+    order: {
+      main: 'Burrito',
+      protein: 'Skirt Steak',
+      rice: 'Purple Rice',
+      sauce: 'Green Crack',
+      toppings: ['Baby Bok Choy', 'Cucumber Kimchi'],
+      drink: 'Thai Iced Tea',
+      cost: 22,
+    },
+    paid: false,
+  },
+  {
+    id: 2,
+    person: 'Tiffany',
+    order: {
+      main: 'Rice Bowl',
+      protein: 'Ginger Chicken',
+      rice: 'Sticky Rice',
+      sauce: 'Korilla',
+      toppings: ['Yuzu Pickled Sweet Pepper', 'Kale'],
+      drink: 'Korchata',
+      cost: 19,
+    },
+    paid: false,
+  },
+  {
+    id: 3,
+    person: 'Brittany',
+    order: {
+      main: 'Salad Bowl',
+      protein: 'Organic Tofu',
+      rice: 'none',
+      sauce: "K'lla",
+      toppings: ['Blue Potato Salad', 'Pico De Gallo', 'Red Kimchi'],
+      drink: 'Sparkling Blood Orange Soda',
+      cost: 20,
+    },
+    paid: false,
+  },
+  {
+    id: 4,
+    person: 'Tylus',
+    order: {
+      main: 'Burrito Bowl',
+      protein: 'Carnitas',
+      rice: 'yellow rice',
+      sauce: "Jalapeno Honey",
+      toppings: ['Grilled Corn', 'Pico De Gallo', 'Guacamole'],
+      drink: 'Pineapple Jarritos',
+      cost: 23,
+    },
+    paid: true,
+  },
+];
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <main>
+        <Receipt receipts={receipts[0]}/>
+        <Receipt receipts={receipts[1]}/>
+        <Receipt receipts={receipts[2]}/>
+        <Receipt receipts={receipts[3]}/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
