01e77f6ce29db5360373176146b88703a8251b1b
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-02-13 19:23:37 -0600

    color change

--- my-app/src/Component/Receipt.jsx
+++ my-app/src/Component/Receipt.jsx
@@ -2,12 +2,12 @@ const Receipt = (props) => {
   return (
     <div>
       <h1>{props.person}</h1>
-      <h2>Main: {props.main}</h2>
-      <h2>Protein: {props.protein}</h2>
-      <h2>Rice: {props.rice}</h2>
-      <h2>Sauce: {props.sauce}</h2>
-      <h2>Drink: {props.drink}</h2>
-      <h2>Cost: ${props.cost}</h2>
+      <h2 className="heading">Main: {props.main}</h2>
+      <h2 className="heading">Protein: {props.protein}</h2>
+      <h2 className="heading">Rice: {props.rice}</h2>
+      <h2 className="heading">Sauce: {props.sauce}</h2>
+      <h2 className="heading">Drink: {props.drink}</h2>
+      <h2 className="heading">Cost: ${props.cost}</h2>
       
 
 
