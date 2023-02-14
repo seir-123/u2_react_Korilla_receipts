566e3b4b9855ac5c1a9112539cf96f044e8e3cc7
mcventus <122248032+mcventus@users.noreply.github.com>
2023-02-14 09:37:34 -0500

    Create Person.jsx

---
+++ korilla/components/Person.jsx
@@ -0,0 +1,16 @@
+function Person (props) {
+    return(
+        <div className="">
+            <div id="person1">
+                {props.person1}
+            </div>
+        <div className="">
+            <div id="person2">
+                {props.person2}
+            </div>
+        </div>
+        </div>
+        
+    )
+}
+export default Person
