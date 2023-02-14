9e875e6f05bc410ec6bb87cf64a04d307fd0d67a
Ariana Jackson <ariana.desiree@gmail.com>
2023-02-13 19:17:20 -0600

    partially done

---
+++ korilla-receipts/src/App.js
@@ -0,0 +1,94 @@
+import './App.css';
+import Receipt from './components/Receipt'
+
+
+const receipts = [
+  {
+    id: 1,
+    person: 'Jeremy',
+    order: {
+      main: 'Burrito',
+      protein: 'Skirt Steak',
+      rice: 'Purple Rice',
+      sauce: 'Green Crack',
+      toppings: ['Baby Bok Choy', 'Cucumber Kimchi'],
+      drink: 'Thai Iced Tea',
+      cost: 22,
+    },
+    paid: false,
+  },
+  {
+    id: 2,
+    person: 'Tiffany',
+    order: {
+      main: 'Rice Bowl',
+      protein: 'Ginger Chicken',
+      rice: 'Sticky Rice',
+      sauce: 'Korilla',
+      toppings: ['Yuzu Pickled Sweet Pepper', 'Kale'],
+      drink: 'Korchata',
+      cost: 19,
+    },
+    paid: false,
+  },
+  {
+    id: 3,
+    person: 'Brittany',
+    order: {
+      main: 'Salad Bowl',
+      protein: 'Organic Tofu',
+      rice: 'none',
+      sauce: "K'lla",
+      toppings: ['Blue Potato Salad', 'Pico De Gallo', 'Red Kimchi'],
+      drink: 'Sparkling Blood Orange Soda',
+      cost: 20,
+    },
+    paid: false,
+  },
+  {
+    id: 4,
+    person: 'Tylus',
+    order: {
+      main: 'Burrito Bowl',
+      protein: 'Carnitas',
+      rice: 'yellow rice',
+      sauce: "Jalapeno Honey",
+      toppings: ['Grilled Corn', 'Pico De Gallo', 'Guacamole'],
+      drink: 'Pineapple Jarritos',
+      cost: 23,
+    },
+    paid: true,
+  },
+];
+
+
+
+const App = () => {
+  return (
+    <div className="App">
+
+      {/* <header className="App-header"></header> */}
+
+      {receipts.map((receipt) => {
+        if (!receipts.paid) {
+          return (
+            <Receipt id={receipt.id}
+                    person={receipt.person}
+                    main={receipt.order.main}
+                    protein={receipt.order.protein}
+                    rice={receipt.order.rice}
+                    sauce={receipt.order.sauce}
+                    drink={receipt.order.drink}
+                    cost={receipt.order.cost}
+                    paid={receipt.order.paid}
+                     />
+          )
+        }
+      })}
+
+    </div>
+  )
+}
+
+
+export default App
---
+++ korilla-receipts/src/components/Receipt.jsx
@@ -0,0 +1,15 @@
+const Receipt = (props) => {
+    return (
+        <div>
+            <h2>Name: {props.person}</h2>
+            <h4>Main: {props.mai}</h4>
+            <h4>Protein: {props.protein}</h4>
+            <h4>Rice: {props.rice}</h4>
+            <h4>Sauce: {props.sauce}</h4>
+            <h4>Drink: {props.drink}</h4>
+            <h5>Cost: {props.cost}</h5>
+        </div>
+    )
+} 
+
+export default Receipt
