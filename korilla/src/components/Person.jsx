eaf6b3572091cc54f1e9435fe516931a697c7b63
mcventus <122248032+mcventus@users.noreply.github.com>
2023-02-14 16:37:21 -0500

    Update Person.jsx

--- korilla/src/components/Person.jsx
+++ korilla/src/components/Person.jsx
@@ -7,8 +7,14 @@ function Person (props) {
             </div>
         <div className="">
             <div id="person2">
-                <div><h1>PART II (Disappears)</h1></div>
+                <div><h1>PART II</h1></div>
                 {props.person2}
+            </div>
+        </div>
+        <div className="p3-cls">
+            <div id="person3">
+                <div><h1>PART II SET VALUE TO TRUE THEN RECEIPT DISAPPEARS</h1></div>
+                {props.person3}
             </div>
         </div>
         </div>
