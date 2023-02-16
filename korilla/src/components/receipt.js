a695faf65a3f6422b86ab179dc6440c282b5b455
Reina Giraldo <ffcmalka@gmail.com>
2023-02-15 21:47:52 -0500

    Final Submission Part 2

--- korilla/src/components/receipt.js
+++ korilla/src/components/receipt.js
@@ -1,18 +1,20 @@
-const receipt = ({person,main,protein,rice,sauce,drink,cost}) => {
+const receipt = (props) => {
+  if(!props.paid){
 return (
     <div className="receipt">
       <div className="receipt-header">
-        <h2>{person}'s Receipt</h2>
+        <h2>{props.person}'s Receipt</h2>
       </div>
       <div className="receipt-order">
-        <p>Main: {main}</p>
-        <p>Protein: {protein}</p>
-        <p>Rice: {rice}</p>
-        <p>Sauce: {sauce}</p>
-        <p>Drink: {drink}</p>
-        <p>Cost: ${cost}</p>
+        <p>Main: {props.main}</p>
+        <p>Protein: {props.protein}</p>
+        <p>Rice: {props.rice}</p>
+        <p>Sauce: {props.sauce}</p>
+        <p>Drink: {props.drink}</p>
+        <p>Cost: ${props.cost}</p>
       </div>
     </div>
-  );
-};
+  )
+}
+}
 export default receipt
