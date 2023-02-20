abecbc06a68b1e8c00eda204a9fe5de90792d326
Yaqub <Yaqubsammy@gmail.com>
2023-02-20 11:56:12 -0600

    Finshed

---
+++ my-app/src/App.js
@@ -0,0 +1,89 @@
+import './App.css';
+import Receipt from './Components/Receipts';
+function App() {
+  const receipts = [
+    {
+      id: 1,
+      person: 'Jeremy',
+      order: {
+        main: 'Burrito',
+        protein: 'Skirt Steak',
+        rice: 'Purple Rice',
+        sauce: 'Green Crack',
+        toppings: ['Baby Bok Choy', 'Cucumber Kimchi'],
+        drink: 'Thai Iced Tea',
+        cost: 22,
+      },
+      paid: false,
+    },
+    {
+      id: 2,
+      person: 'Tiffany',
+      order: {
+        main: 'Rice Bowl',
+        protein: 'Ginger Chicken',
+        rice: 'Sticky Rice',
+        sauce: 'Korilla',
+        toppings: ['Yuzu Pickled Sweet Pepper', 'Kale'],
+        drink: 'Korchata',
+        cost: 19,
+      },
+      paid: false,
+    },
+    {
+      id: 3,
+      person: 'Brittany',
+      order: {
+        main: 'Salad Bowl',
+        protein: 'Organic Tofu',
+        rice: 'none',
+        sauce: "K'lla",
+        toppings: ['Blue Potato Salad', 'Pico De Gallo', 'Red Kimchi'],
+        drink: 'Sparkling Blood Orange Soda',
+        cost: 20,
+      },
+      paid: false,
+    },
+    {
+      id: 4,
+      person: 'Tylus',
+      order: {
+        main: 'Burrito Bowl',
+        protein: 'Carnitas',
+        rice: 'yellow rice',
+        sauce: "Jalapeno Honey",
+        toppings: ['Grilled Corn', 'Pico De Gallo', 'Guacamole'],
+        drink: 'Pineapple Jarritos',
+        cost: 23,
+      },
+      paid: true,
+    },
+  ];
+
+  const unpaidReceipts = receipts.filter((receipt) => !receipt.paid);
+
+  return (
+    <div className="App">
+      <h1>Korilla</h1>
+      <header className="App-header">
+        {unpaidReceipts.map((receipt) => (
+          <div className="list" key={receipt.id}>
+            <Receipt
+              person={receipt.person}
+              order={receipt.order}
+              main={receipt.order.main}
+              protein={receipt.order.protein}
+              rice={receipt.order.rice}
+              sauce={receipt.order.sauce}
+              drink={receipt.order.drink}
+              cost={receipt.order.cost}
+              status={receipt.paid}
+            />
+          </div>
+        ))}
+      </header>
+    </div>
+  );
+}
+
+export default App;
