5ac4887d07dcbce91dbb36c7fef044bc7730d955
Elliot <ellthrowaway2@icloud.com>
2023-06-20 16:41:59 -0400

    implemented conditional rendering of unpaid receipts using map array method

--- korilla-receipts/src/App.jsx
+++ korilla-receipts/src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Receipt from './components/Receipt';
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const receipts = [
+    {
+      id: 1,
+      person: 'Jeremy',
+      order: {
+        main: 'Burrito',
+        protein: 'Skirt Steak',
+        rice: 'Purple Rice',
+        sauce: 'Green',
+        toppings: ['Baby Bok Choy', 'Cucumber Kimchi'],
+        drink: 'Thai Iced Tea',
+        cost: 22,
+      },
+      paid: false,
+    },
+    {
+      id: 2,
+      person: 'Tiffany',
+      order: {
+        main: 'Rice Bowl',
+        protein: 'Ginger Chicken',
+        rice: 'Sticky Rice',
+        sauce: 'Korilla',
+        toppings: ['Yuzu Pickled Sweet Pepper', 'Kale'],
+        drink: 'Korchata',
+        cost: 19,
+      },
+      paid: false,
+    },
+    {
+      id: 3,
+      person: 'Brittany',
+      order: {
+        main: 'Salad Bowl',
+        protein: 'Organic Tofu',
+        rice: 'none',
+        sauce: "K'lla",
+        toppings: ['Blue Potato Salad', 'Pico De Gallo', 'Red Kimchi'],
+        drink: 'Sparkling Blood Orange Soda',
+        cost: 20,
+      },
+      paid: false,
+    },
+    {
+      id: 4,
+      person: 'Tylus',
+      order: {
+        main: 'Burrito Bowl',
+        protein: 'Carnitas',
+        rice: 'yellow rice',
+        sauce: "Jalapeno Honey",
+        toppings: ['Grilled Corn', 'Pico De Gallo', 'Guacamole'],
+        drink: 'Pineapple Jarritos',
+        cost: 23,
+      },
+      paid: true,
+    },
+  ];
+  let paidStatus;
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {receipts.map((receipt) => {
+        if (receipt.paid === false) {
+          return (
+            <Receipt person={receipt.person} order={receipt.order} />
+          )
+        } else {
+          return null
+        }
+      })
+      }
     </>
   )
 }
-
 export default App
