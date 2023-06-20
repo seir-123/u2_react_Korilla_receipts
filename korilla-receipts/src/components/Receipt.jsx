c78b4817b7ee68324fb2b10aefe5a658abb9e7d6
Elliot <ellthrowaway2@icloud.com>
2023-06-20 15:59:07 -0400

    some styling

--- korilla-receipts/src/components/Receipt.jsx
+++ korilla-receipts/src/components/Receipt.jsx
@@ -1,14 +1,14 @@
 export default function Receipt(props) {
     return (
-        <div>
+        <div className="receipt-card">
             <h1> {props.person} </h1>
             <ul>
-                <li> Main: {props.order.main} </li>
-                <li> Protein: {props.order.protein} </li>
-                <li> Rice: {props.order.rice} </li>
-                <li> Sauce: {props.order.sauce} </li>
-                <li> Drink: {props.order.drink} </li>
-                <li> Cost: {props.order.cost} </li>
+                <li><span className="label"> Main:</span> {props.order.main} </li>
+                <li><span className="label"> Protein:</span> {props.order.protein} </li>
+                <li><span className="label"> Rice:</span> {props.order.rice} </li>
+                <li><span className="label"> Sauce:</span> {props.order.sauce} </li>
+                <li><span className="label"> Drink:</span> {props.order.drink} </li>
+                <li><span className="label"> Cost:</span> ${props.order.cost} </li>
             </ul>
         </div>
     )
